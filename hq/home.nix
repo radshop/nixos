eae52e5c05362ebf999984971c06fdcf06a1b676
Myron Weber <myron@radshop.com>
2023-08-24 10:32:10 -0700

    nixhq: generation 70

--- hq/home.nix
+++ hq/home.nix
@@ -71,13 +71,25 @@
 
   programs.tmux = {
     enable = true;
-    plugins = with pkgs;
-    [ tmuxPlugins.sensible
-      tmuxPlugins.resurrect
+    plugins = with pkgs.tmuxPlugins; [
+      sensible
+      resurrect
       {
-        plugin = tmuxPlugins.continuum;
+        plugin = resurrect;
+        extraConfig = ''
+          set -g @resurrect-strategy-nvim 'session'
+          set -g @resurrect-strategy-vim 'session'
+          set -g @resurrect-capture-pane-contents 'on'
+          resurrect_dir="$HOME/.tmux/resurrect"
+          set -g @resurrect-dir $resurrect_dir
+          set -g @resurrect-hook-post-save-all 'target=$(readlink -f $resurrect_dir/last); sed "s| --cmd .*-vim-pack-dir||g; s|/etc/profiles/per-user/$USER/bin/||g" $target | sponge $target'
+        '';
+      }
+      {
+        plugin = continuum;
         extraConfig = ''
           set -g @continuum-restore 'on'
+          set -g @continuum-save-interval '10'
           '';
       }
     ];

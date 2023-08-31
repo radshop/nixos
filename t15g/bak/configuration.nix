310ac77dcabeeb52847526f47f4e23943c4751a3
radshop <myron@radshop.com>
2023-08-31 07:00:59 -0700

    nixt15g: generation 38

---
+++ t15g/bak/configuration.nix
@@ -0,0 +1,206 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixt15g"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Los_Angeles";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.miscguy = {
+    isNormalUser = true;
+    description = "miscguy";
+    extraGroups = [ "networkmanager" "wheel" "libvirtd" "docker"];
+    packages = with pkgs; [
+      firefox librewolf brave ungoogled-chromium
+    #  thunderbird
+    ];
+  };
+
+  security.sudo.extraRules = [
+    {
+      users = [ "miscguy" ];
+      commands = [
+        {
+          command = "ALL";
+          options = [ "SETENV" "NOPASSWD" ];
+        }
+      ];
+    }
+  ];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git wget
+    mullvad-vpn
+    (import ./vim.nix)
+    virt-manager
+    tmux
+    zoom-us
+    pv
+    calibre
+    teams
+    onedrive
+  ];
+
+  virtualisation = {
+    # libvirt/qemu/kvm enable
+    libvirtd.enable = true;
+    # docker
+    docker = {
+      enable = true;
+    };
+  };
+  # dconf for kvm
+  programs.dconf.enable = true;
+
+  # ssh keyring
+  programs.sway.extraSessionCommands = ''
+      eval $(gnome-keyring-daemon --start --components=pkcs11,secrets,ssh);
+      export SSH_AUTH_SOCK;
+  '';
+
+  services.postgresql = {
+    enable = true;
+    authentication = pkgs.lib.mkForce ''
+    # Generated file; do not edit!
+    # TYPE  DATABASE        USER            ADDRESS                 METHOD
+    local   all             all                                     trust
+    host    all             all             127.0.0.1/32            trust
+    host    all             all             ::1/128                 trust
+    '';
+  };
+  services.onedrive.enable = true;
+  services.mullvad-vpn.enable = true;
+  services.xserver = {
+    # Configure keymap in X11
+    layout = "us";
+    xkbVariant = "";
+    # Enable the X11 windowing system.
+    enable = true;
+    # displaylink
+    videoDrivers = [ "displaylink" "modesetting" ];
+    # Enable the GNOME Desktop Environment.
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+  };
+  services.syncthing = {
+      enable = true;
+      user = "miscguy";
+      dataDir = "/home/miscguy/sync";    # Default folder for new synced folders
+      configDir = "/home/miscguy/.config/syncthing";   # Folder for Syncthing's settings and keys
+  };
+
+  # flatpaks
+  services.flatpak.enable = true;
+  # flatpak install flathub org.signal.Signal
+  # flatpak install flathub com.amazon.Workspaces
+
+  programs.tmux = {
+    enable = true;
+    extraConfig = ''
+      run-shell ${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect/resurrect.tmux
+      run-shell ${pkgs.tmuxPlugins.continuum}/share/tmux-plugins/continuum/continuum.tmux
+      set -g @continuum-restore 'on'
+    '';
+  };
+
+  networking.extraHosts =
+    ''
+      172.17.0.2 sql1
+    '';
+
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+}

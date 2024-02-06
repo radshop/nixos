6b3ba430692fb613c00a2e89958ea2cb5d2a19dc
Myron Weber <myron@radshop.com>
2024-02-06 10:34:16 -0800

    nixhq: generation 104

--- hq/home.nix
+++ hq/home.nix
@@ -8,7 +8,7 @@
   home.stateVersion = "23.05";
   nixpkgs.config.allowUnfree = true;
   home.packages = with pkgs; [
-    firefox librewolf brave chromium google-chrome vivaldi
+    firefox librewolf chromium google-chrome vivaldi
     # logseq
     vlc
     libreoffice-fresh

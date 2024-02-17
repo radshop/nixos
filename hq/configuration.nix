5cf41c7026ca241907e22485e6064c8899cb3a07
Myron Weber <myron@radshop.com>
2024-02-17 15:15:30 -0800

    nixhq: generation 112

--- hq/configuration.nix
+++ hq/configuration.nix
@@ -9,6 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       <home-manager/nixos>
+      ../shared/locale.nix
       ../shared/services.nix
     ];
 
@@ -19,35 +20,8 @@
   networking.hostName = "nixhq"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
   networking.networkmanager.enable = true;
-
-  # enable nft firewall
-  # networking.nftables = {
-  #  enable = true;
-  # };
-
-  # Set your time zone.
-  time.timeZone = "America/Los_Angeles";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
 
   # Enable sound with pipewire.
   sound.enable = true;
@@ -152,8 +126,8 @@
   services.onedrive.enable = true;
   services.xserver = {
     # Configure keymap in X11
-    layout = "us";
-    xkbVariant = "";
+    xkb.layout = "us";
+    xkb.variant = "";
     # Enable the X11 windowing system.
     enable = true;
     # displaylink

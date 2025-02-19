c347985bfca7e954a3a1daee75c6785d12b3e479
Myron Weber <myron@radshop.com>
2025-02-18 18:29:29 -0800

    nixhq: generation 325

--- hq/configuration.nix
+++ hq/configuration.nix
@@ -81,7 +81,7 @@
   #gnome remote desktop
   services.gnome.gnome-remote-desktop.enable = true;
   services.xrdp.enable = true;
-  services.xrdp.defaultWindowManager = "${pkgs.gnome.gnome-session}/bin/gnome-session";
+  #services.xrdp.defaultWindowManager = "${pkgs.gnome.gnome-session}/bin/gnome-session";
 
 
   # fuse
